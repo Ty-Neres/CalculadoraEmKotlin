0b4bd3b54000e1a3ba980faa1fadf6e406724525
Tiago Douglas <neres70off@gmail.com>
2023-11-15 23:33:07 -0300

    basic calculator

--- app/src/main/java/com/example/calculadoraemkotlin/MainActivity3.kt
+++ app/src/main/java/com/example/calculadoraemkotlin/MainActivity3.kt
@@ -4,7 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
+private lateinit var valor1EditText: EditText
+private lateinit var valor2EditText: EditText
+
+private  lateinit var resultadoTextView: TextView
+var operacaoSelecionada : String ? = null
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +23,43 @@ class MainActivity3 : AppCompatActivity() {
             basicoParaInicio()
         })
 
+        valor1EditText = findViewById(R.id.valor1)
+        valor2EditText = findViewById(R.id.valor2)
+
+        resultadoTextView = findViewById(R.id.resultado)
+
+        val somaButton : Button = findViewById(R.id.soma)
+        val subtracaoButton : Button = findViewById(R.id.subtracao)
+        val divisaoButton : Button = findViewById(R.id.divisao)
+        val multicacaoButton : Button = findViewById(R.id.multiplicacao)
+
+        val enterButton : Button = findViewById(R.id.enter)
+
+        somaButton.setOnClickListener{operacao("+")}
+        subtracaoButton.setOnClickListener{operacao("-") }
+        divisaoButton.setOnClickListener{operacao("/")}
+        multicacaoButton.setOnClickListener{operacao("*")}
+
+        enterButton.setOnClickListener{calcular()}
+    }
+
+    private fun calcular() {
+        val valor1 = valor1EditText.text.toString().toDoubleOrNull()?: 0.0
+        val valor2 = valor2EditText.text.toString().toDoubleOrNull()?: 0.0
+
+        val resultado : Double = when(operacaoSelecionada){
+
+            "+" -> valor1 + valor2
+            "-" -> valor1 - valor2
+            "*" -> valor1 * valor2
+            "/" -> if (valor2 != 0.0) valor1 / valor2 else Double.NaN
+            else -> Double.NaN
+        }
+        resultadoTextView.text = "$valor1 $operacaoSelecionada $valor2 = $resultado"
+    }
+
+    private fun operacao(operacao: String) {
+    operacaoSelecionada = operacao
     }
 
 
